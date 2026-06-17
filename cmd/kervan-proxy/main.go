a8a9614e8e13eeb0411b55963ab0642eaf802159
Yavuz Selim Bayram <ys.bayram@outlook.com>
2026-06-18 01:25:43 +0300

    feat(p9): rewrite main.go as full server entry point with graceful shutdown; add integration tests

--- cmd/kervan-proxy/main.go
+++ cmd/kervan-proxy/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ysBayram/kervan-proxy/internal/server"
+)
 
 func main() {
-	fmt.Println("kervan-proxy: resilient stream pipeline SDK")
+	cfg := server.ConfigFromFlags()
+	srv := server.NewProxyServer(cfg)
+
+	if err := srv.Start(); err != nil {
+		log.Fatalf("server: failed to start: %v", err)
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+
+	srv.Stop()
 }
