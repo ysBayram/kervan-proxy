369f8ee78d28cf9c72adfe85aa54088979f03f3a
Yavuz Selim Bayram <ys.bayram@outlook.com>
2026-07-10 12:36:45 +0300

    refactor(ws): improve formatting and readability in handleWS function

--- internal/server/proxy.go
+++ internal/server/proxy.go
@@ -164,38 +164,38 @@ func (s *ProxyServer) handleWS(w http.ResponseWriter, r *http.Request) {
 		s.activeConns.Done()
 	}()
 
-  var up upstream
-  if wsMode {
-      upURL, uerr := s.buildUpstreamURL(r)
-      if uerr != nil {
-          log.Printf("server: bad upstream url: %v", uerr)
-          return
-      }
-      // Build X-Forwarded-* headers (append semantics)
-      hdr := http.Header{}
-      if prior := r.Header.Get("X-Forwarded-Host"); prior != "" {
-          hdr.Set("X-Forwarded-Host", prior+", "+r.Host)
-      } else {
-          hdr.Set("X-Forwarded-Host", r.Host)
-      }
-      if ip, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
-          if prior := r.Header.Get("X-Forwarded-For"); prior != "" {
-              hdr.Set("X-Forwarded-For", prior+", "+ip)
-          } else {
-              hdr.Set("X-Forwarded-For", ip)
-          }
-      }
-      if r.TLS != nil {
-          hdr.Set("X-Forwarded-Proto", "https")
-      } else {
-          hdr.Set("X-Forwarded-Proto", "http")
-      }
+	var up upstream
+	if wsMode {
+		upURL, uerr := s.buildUpstreamURL(r)
+		if uerr != nil {
+			log.Printf("server: bad upstream url: %v", uerr)
+			return
+		}
+		// Build X-Forwarded-* headers (append semantics)
+		hdr := http.Header{}
+		if prior := r.Header.Get("X-Forwarded-Host"); prior != "" {
+			hdr.Set("X-Forwarded-Host", prior+", "+r.Host)
+		} else {
+			hdr.Set("X-Forwarded-Host", r.Host)
+		}
+		if ip, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+			if prior := r.Header.Get("X-Forwarded-For"); prior != "" {
+				hdr.Set("X-Forwarded-For", prior+", "+ip)
+			} else {
+				hdr.Set("X-Forwarded-For", ip)
+			}
+		}
+		if r.TLS != nil {
+			hdr.Set("X-Forwarded-Proto", "https")
+		} else {
+			hdr.Set("X-Forwarded-Proto", "http")
+		}
 
-      up = NewResilientWSUpstream(s.ctx, upURL, websocket.Subprotocols(r), hdr, s.cfg.UpstreamTimeout, s.cfg.ReconnectTimeout)
-  } else {
-      up = NewResilientUpstream(s.ctx, s.cfg.UpstreamAddr, s.cfg.UpstreamTimeout, s.cfg.ReconnectTimeout)
-  }
-  
+		up = NewResilientWSUpstream(s.ctx, upURL, websocket.Subprotocols(r), hdr, s.cfg.UpstreamTimeout, s.cfg.ReconnectTimeout)
+	} else {
+		up = NewResilientUpstream(s.ctx, s.cfg.UpstreamAddr, s.cfg.UpstreamTimeout, s.cfg.ReconnectTimeout)
+	}
+
 	defer up.Close()
 
 	s.runPipelines(clientReader, clientWriter, up)
